93344e89ca9674cbc14b8a42c53670b2dad47158
Andrew Auclair <mightymalakai33@gmail.com>
2023-10-09 18:17:26 -0400

    2018 day 2.

--- advent_of_code/2018.cpp
+++ advent_of_code/2018.cpp
@@ -58,3 +58,83 @@ bool aoc_2018_1::part2()
 
 	return total == 72889;
 }
+
+bool aoc_2018_2::part1()
+{
+	auto file = open_input_file(2018, 2);
+
+	const std::vector<std::string> lines = read_lines(file);
+
+	int lines_with_2_chars = 0;
+	int lines_with_3_chars = 0;
+
+	for (const std::string& line : lines)
+	{
+		bool counted_2 = false;
+		bool counted_3 = false;
+
+		for (char ch : line)
+		{
+			auto count = std::count(line.begin(), line.end(), ch);
+
+			if (!counted_2 && count == 2)
+			{
+				lines_with_2_chars++;
+				counted_2 = true;
+			}
+			if (!counted_3 && count == 3)
+			{
+				lines_with_3_chars++;
+				counted_3 = true;
+			}
+		}
+	}
+
+	return (lines_with_2_chars * lines_with_3_chars) == 8118;
+}
+
+bool aoc_2018_2::part2()
+{
+	auto file = open_input_file(2018, 2);
+
+	const std::vector<std::string> lines = read_lines(file);
+
+	std::string answer;
+
+	for (int i = 0; i < lines.size(); i++)
+	{
+		for (int j = i + 1; j < lines.size(); j++)
+		{
+			int diffs = 0;
+
+			for (int k = 0; k < std::min(lines[i].length(), lines[j].length()); k++)
+			{
+				if (lines[i][k] != lines[j][k])
+				{
+					diffs++;
+				}
+				if (diffs > 1)
+				{
+					break;
+				}
+			}
+			if (diffs == 1)
+			{
+				for (int k = 0; k < std::min(lines[i].length(), lines[j].length()); k++)
+				{
+					if (lines[i][k] == lines[j][k])
+					{
+						answer += lines[i][k];
+					}
+				}
+				break;
+			}
+		}
+		if (!answer.empty())
+		{
+			break;
+		}
+	}
+	
+	return answer == "jbbenqtlaxhivmwyscjukztdp";
+}
